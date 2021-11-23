aa8348a2b4e57b6f5d133564b8623b11307ef36b
Celina Alzenor <you@example.com>
2021-11-23 15:08:40 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <sstream>
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+//returns the result of two numbers based on the operation
+int calculate(const string& operation, int x, int y)
+{
+    //addition
+    if(operation =="+")
+    {
+      return x+y;
+    }
+
+    //subtraction
+    else if(operation =="-")
+    {
+        return x-y;
+    }
+
+    //division
+    else if(operation =="/")
+    {
+        return x/y;
+    }
+
+    //multiplication
+    else if(operation =="/")
+    {
+        return x*y;
+    }
+
+    //if user inputs something that's not a valid operation
+    else
+    {
+        cout << "Not a valid operation!";
+        return 0;
+    }
+}
+
+int main() {
+
+    string operation;
+    int x;
+    int y;
+
+    //user inputs the operations and 2 numbers
+    cin >> operation >> x >> y;
+
+    //if user inputted two numbers, run the program
+    if(isdigit(x) && isdigit(y))
+    {
+        //prints the result
+        int result = calculate(operation, x,y);
+        if( result != 0)
+        {
+            cout << result;
+        }
+    }
+
+    //remind user what to input
+    else
+    {
+        cout << "Please input integers";
+    }
+    return 0;
+}
